de1def5cad42d95bac033f0982d0403b672d0328
Adam Östgaard <adam.ostgaard@gmail.com>
2018-10-18 15:33:02 +0200

    add board and position

---
+++ INTE-Project/src/main/java/com/Grupp25/app/board/Board.java
@@ -0,0 +1,5 @@
+package com.Grupp25.app.board;
+
+class Board {
+
+}
---
+++ INTE-Project/src/main/java/com/Grupp25/app/board/Position.java
@@ -0,0 +1,22 @@
+package com.Grupp25.app.board;
+
+class Position {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int value) {
+        x = value;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int value) {
+        x = value;
+    }
+}
