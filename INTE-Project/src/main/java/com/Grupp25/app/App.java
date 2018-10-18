9f83a3a6c24c3909f8615137c486bd49ba30c8ab
Adam Östgaard <adam.ostgaard@gmail.com>
2018-10-18 14:19:04 +0200

    add maven and initial files

---
+++ INTE-Project/src/main/java/com/Grupp25/app/App.java
@@ -0,0 +1,13 @@
+package com.Grupp25.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is app!" );
+    }
+}
