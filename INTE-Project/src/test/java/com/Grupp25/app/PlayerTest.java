e71c1d3b5ffc8b7141bb51d9d8cf57a017969d5b
niklasfriberg <niklasfribergprojekt@gmail.com>
2018-10-19 12:03:40 +0200

    Fixed playertest class declaration

--- INTE-Project/src/test/java/com/Grupp25/app/PlayerTest.java
+++ INTE-Project/src/test/java/com/Grupp25/app/PlayerTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-class PlayerTest {
+public class PlayerTest {
     @Test
     public void testLevelUp() {
         Player player = new Player();
