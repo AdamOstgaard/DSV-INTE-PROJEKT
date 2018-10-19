63b84374a3b0595a9ea5433ef86bda6284a27bc1
niklasfriberg <niklasfribergprojekt@gmail.com>
2018-10-19 13:05:41 +0200

    Added positiontests

--- INTE-Project/src/test/java/com/Grupp25/app/board/PositionTest.java
+++ INTE-Project/src/test/java/com/Grupp25/app/board/PositionTest.java
@@ -1,8 +1,11 @@
 package com.Grupp25.app.board;
 
+import org.junit.rules.ExpectedException;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
 
 public class PositionTest extends TestCase {
     public PositionTest(String testName) {
@@ -30,5 +33,27 @@ public class PositionTest extends TestCase {
         pos.setY(2);
         int y = pos.getY();
         assertTrue(y == 2);
+    }
+
+    @org.junit.Test(expected = Exception.class)
+    public void testNegativePositionXValue() {
+        new Position(-1, 3);
+    }
+
+    @org.junit.Test(expected = Exception.class)
+    public void testNegativePositionYValue() {
+        new Position(5, -1);
+    }
+
+    @org.junit.Test(expected = Exception.class)
+    public void testSetNegativeX() {
+        Position pos = new Position(4, 4);
+        pos.setX(-2);
+    }
+
+    @org.junit.Test(expected = Exception.class)
+    public void testSetNegativeY() {
+        Position pos = new Position(10, 5);
+        pos.setY(-2);
     }
 }
