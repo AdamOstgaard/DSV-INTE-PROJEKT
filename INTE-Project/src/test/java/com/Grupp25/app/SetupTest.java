6d90a36ddb2ba61d5f5a4560834806951a4ac105
niklasfriberg <niklasfribergprojekt@gmail.com>
2018-10-23 21:25:21 +0200

    Added setup class and setup testing class

---
+++ INTE-Project/src/test/java/com/Grupp25/app/SetupTest.java
@@ -0,0 +1,38 @@
+package com.Grupp25.app;
+
+import com.Grupp25.app.Setup;
+
+import static org.junit.Assert.*;
+
+import java.lang.instrument.IllegalClassFormatException;
+import java.util.IllegalFormatException;
+
+import org.junit.Test;
+import org.junit.Before;
+
+
+public class SetupTest {
+
+    Setup setup;
+
+    @Before
+    public void setUp(){
+        setup = new Setup();
+    }
+
+    // @Test (expected = Exception.class)
+    // public void illegalCharactersInName(){
+
+    // }
+
+    // @Test (expected = NullPointerException.class)
+    // public void nameIsNull(){
+
+    // }
+
+    // @Test (expected = Exception.class)
+    // public void nameIsTooLong(){
+
+    // }
+
+}
